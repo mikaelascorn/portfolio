105e7b2f0786166e810ca728aba6e52107a610b8
Mikaela Scornaienchi <scornaim@gmail.com>
2018-06-02 10:19:41 -0400

    styles

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,11 +1,21 @@
 $(function () {
   console.log('hello');
 
-
   $('.toggle').click(function () {
     console.log('clicked');
     $('.toggle').toggleClass('active');
     $('ul').toggleClass('active');
+
+    const ul = $('ul');
+    if (ul.hasClass('active')) {
+      ul
+        .fadeIn('ul')
+        // .addClass('fa-times');
+    } else {
+      ul
+        .fadeOut('ul')
+        // .addClass('fa-bars');
+    }
 
   })
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,6 +7,15 @@ $(function () {
     console.log('clicked');
     $('.toggle').toggleClass('active');
     $('ul').toggleClass('active');
+
+    var ul = $('ul');
+    if (ul.hasClass('active')) {
+      ul.fadeIn('ul');
+      // .addClass('fa-times');
+    } else {
+      ul.fadeOut('ul');
+      // .addClass('fa-bars');
+    }
   });
 
   $('a').smoothScroll({
